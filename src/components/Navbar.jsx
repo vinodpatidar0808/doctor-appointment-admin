dfbbc0e432167269595400e7023e5b350b645356
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-11-03 14:14:07 +0530

    Fix dentist form and navigation background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const { pathname } = useLocation();
 
   return (
-    <nav className="flex items-center justify-between  border-b-2 border-charcoalGray sticky top-0 text-charcoalGray px-8 py-5  w-full">
+    <nav className="flex items-center justify-between bg-white  border-b-2 border-charcoalGray sticky top-0 text-charcoalGray px-8 py-5  w-full">
       <p className="ml-[22%]">{getPageHeader(pathname)}</p>
       <div className="flex  gap-3">
         <p>Admin</p>
