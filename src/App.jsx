3be3d5a117fd8585661e0eef3081dc852e45d9e3
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-11-10 00:53:52 +0530

    private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import MainLayout from "./components/MainLayout"
 import Dashboard from "./pages/Dashboard"
+import Dentist from "./pages/Dentist"
 import Login from "./pages/Login"
+import PrivateRoute from "./pages/PrivateRoute"
 import Reports from "./pages/Reports"
 import Services from "./pages/Services"
-import Dentist from "./pages/Dentist"
-import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -16,12 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
 
-        <Route path="/" element={<MainLayout />}>
-          {/* Nested routes */}
-          <Route index path="dashboard" element={<Dashboard />} /> 
-          <Route path="services" element={<Services />} />
-          <Route path="reports" element={<Reports />} />
-          <Route path="dentist" element={<Dentist />} />
+        <Route path="/" element={<PrivateRoute> <MainLayout /></PrivateRoute>}>
+          <Route index path="dashboard" element={<PrivateRoute> <Dashboard /></PrivateRoute>} />
+          <Route path="services" element={<PrivateRoute>  <Services /></PrivateRoute>} />
+          <Route path="reports" element={<PrivateRoute> <Reports /></PrivateRoute>} />
+          <Route path="dentist" element={<PrivateRoute> <Dentist /></PrivateRoute>} />
         </Route>
       </Routes>
       <ToastContainer />
